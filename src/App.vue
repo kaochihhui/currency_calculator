d08eee97377283136aa43b05b799e12abb3ee124
Chih Hui Kao <kchforwork@gmail.com>
2018-10-07 15:52:29 +0800

    add dropdown icon

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="app">
     <v-app>
-      <v-navigation-drawer app>
+      <v-toolbar app>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn flat><router-link to="/">Prices</router-link></v-btn>
+          <v-btn flat><router-link to="/wallet">Wallet</router-link></v-btn>
+          <v-btn flat><router-link to="/account">Account</router-link></v-btn>
+          <v-flex xs12>
+            <v-select
+              :items="items"
+              :item-value ="itemValue"
+              label="English"
+              outline solo
+            ></v-select>
+          </v-flex>
+        </v-toolbar-items>
+      </v-toolbar>
+      <!-- <v-navigation-drawer app>
         <router-link to="/">Prices</router-link> |
         <router-link to="/wallet">Wallet</router-link> |
         <router-link to="/account">Account</router-link>
-      </v-navigation-drawer>
+      </v-navigation-drawer> -->
       <v-content>
         <v-container fluid>
           <v-fade-transition mode="out-in">
-            <router-view/>  
+            <router-view/>
           </v-fade-transition>
         </v-container>
       </v-content>
@@ -25,6 +41,15 @@
   </div>
 
 </template>
+<script>
+export default {
+  name: 'app',
+  data: () => ({
+    items: ['繁體中文', 'English'],
+    itemValue: ['zh-TW','en-US']
+  })
+}
+</script>
 
 <style lang="scss">
 // #app {
